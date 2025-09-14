785efae500dc08bb93926333af9510e2ca8c3c58
Ivrikidivri <nownevergirl@gmail.com>
2025-09-14 14:19:57 +0700

    Update design.js

--- design.js
+++ design.js
@@ -81,6 +81,21 @@ document.addEventListener("DOMContentLoaded", () => {
   `;
   })();*/
 
+
+    // === Load Footer dynamically ===
+    (() => {
+      const footerContainer = document.getElementById("footer");
+      if (!footerContainer) return;
+
+    fetch("footer.html")
+      .then(res => res.text())
+      .then(data => {
+        footerContainer.innerHTML = data;
+      })
+    .catch(err => console.error("Footer load error:", err));
+  })();
+
+      
       // === Mobile collapsible submenus ===
       const submenuParents = document.querySelectorAll(".has-submenu > a");
       submenuParents.forEach(link => {
@@ -103,31 +118,8 @@ document.addEventListener("DOMContentLoaded", () => {
     .catch(err => console.error("Navbar load error:", err));
 })();
 
-
-// === Load Footer dynamically ===
-  (() => {
-    const footerContainer = document.getElementById("footer");
-    if (!footerContainer) return;
-
-    fetch("footer.html")
-      .then(res => res.text())
-      .then(data => {
-        footerContainer.innerHTML = data;
-      })
-      .catch(err => {
-        console.error("Footer load error:", err);
-        // Fallback if footer.html fails
-        footerContainer.innerHTML = `
-          <footer>
-            <p>&copy; ${new Date().getFullYear()} EAPacRC Secretariat. All rights reserved.</p>
-          </footer>
-        `;
-      });
-  })();
+  
 });
-
-
-
 
 // === Gallery Slider ===
   document.querySelectorAll('.gallery-title, .img-gallery')
@@ -188,6 +180,7 @@ function prevImage() {
     closeFullImg();
   }
 }
+
 
 
 
